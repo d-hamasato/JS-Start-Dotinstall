786fbb97151dd465bb40d64a37048a70580df66f
d-hamasato <73775819+d-hamasato@users.noreply.github.com>
2021-04-17 22:32:21 +0900

    [finish] JSオブジェクト 28-35

--- JavaScriptObject/js/main.js
+++ JavaScriptObject/js/main.js
@@ -357,3 +357,147 @@
 // }
 
 // #28
+// {
+//   const posts = [
+//     {
+//       text: 'JavaScriptの勉強中...',
+//       likeCount: 0,
+//     },
+//     {
+//       text: 'プログラミング楽しい！',
+//       likeCount: 0,
+//     },
+//   ];
+
+//   function show(post) {
+//     console.log(`${post.text} - ${post.likeCount}いいね`);
+//   }
+
+//   show(posts[0]);
+// }
+
+// #29
+// {
+//   const posts = [
+//     {
+//       text: 'JavaScriptの勉強中...',
+//       likeCount: 0,
+      // show:   function() {
+      //   console.log(`${this.text} - ${this.likeCount}いいね`);
+      // },
+//       show() {
+//         console.log(`${this.text} - ${this.likeCount}いいね`);
+//       },
+//     },
+//     {
+//       text: 'プログラミング楽しい！',
+//       likeCount: 0,
+//       show() {
+//         console.log(`${this.text} - ${this.likeCount}いいね`);
+//       },
+//     },
+//   ];
+
+//   // show(posts[0]);
+//   posts[0].show();
+//   posts[1].show();
+// }
+
+// #30, 31, 32
+// {
+//   class Post {
+//     constructor(text) {
+//       this.text = text;
+//       this.likeCount = 0;
+//     }
+//     show() {
+//       console.log(`${this.text} - ${this.likeCount} likes`);
+//     }
+
+//     like() {
+//       this.likeCount++;
+//       this.show();
+//     }
+//   }
+
+//   const posts = [
+//     new Post('JavaScriptの勉強中...'),
+//     new Post('プログラミング楽しい！'),
+//   ];
+
+//   posts[0].like();
+
+  // posts[0].show();
+  // posts[1].show();
+// }
+
+// #33
+// {
+//   class Post {
+//     constructor(text) {
+//       this.text = text;
+//       this.likeCount = 0;
+//     }
+//     show() {
+//       console.log(`${this.text} - ${this.likeCount} likes`);
+//     }
+
+//     like() {
+//       this.likeCount++;
+//       this.show();
+//     }
+
+//     // 静的メソッド
+//     // thisは使えない
+//     static showInfo() {
+//       console.log('Post class version 1.0');
+//     }
+//   }
+
+//   const posts = [
+//     new Post('JavaScriptの勉強中...'),
+//     new Post('プログラミング楽しい！'),
+//   ];
+
+//   // posts[0].like();
+//   Post.showInfo();
+// }
+
+// #34, 35
+{
+  class Post {
+    constructor(text) {
+      this.text = text;
+      this.likeCount = 0;
+    }
+    show() {
+      console.log(`${this.text} - ${this.likeCount} likes`);
+    }
+
+    like() {
+      this.likeCount++;
+      this.show();
+    }
+  }
+
+  class SponsoredPost extends Post {
+    constructor(text, sponsor) {
+      super(text);
+      this.sponsor = sponsor;
+    }
+
+    show() {
+      super.show();
+      console.log(`...sponsored by ${this.sponsor}`);
+    }
+  }
+
+  const posts = [
+    new Post('JavaScriptの勉強中...'),
+    new Post('プログラミング楽しい！'),
+    new SponsoredPost('3分動画でマスターしよう', 'dotinstall'),
+  ];
+
+  posts[2].show();
+  posts[2].like();
+}
